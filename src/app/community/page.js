ef057f83a3f26684fd5990347d89137ac4db6582
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-06-15 19:08:39 +0300

    done

---
+++ src/app/community/page.js
@@ -0,0 +1,11 @@
+import CommunityIcons from "@/components/community/CommunityIcons";
+import CommunityInfo from "@/components/community/CommunityInfo";
+
+export default function CommunityPage() {
+    return (
+        <div className="mt-8">
+            <CommunityInfo/>
+            <CommunityIcons/>
+        </div>
+    )
+}
