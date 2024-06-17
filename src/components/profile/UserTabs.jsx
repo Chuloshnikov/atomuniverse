5e0d8f31ce262aad7b969c2062b23d01ec68266e
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-06-17 21:16:49 +0300

    done

--- src/components/profile/UserTabs.jsx
+++ src/components/profile/UserTabs.jsx
@@ -12,26 +12,26 @@ const UserTabs = ({ isAdmin }) => {
         >
             <Link 
             href={'/profile'} 
-            className={`border-2 border-mainText xs:rounded-t-xl mdl:rounded-r-none mdl:rounded-l-xl ${path === '/profile' ? 'active' : ''}`}>
+            className={`border-2 border-accentBg xs:rounded-t-xl mdl:rounded-r-none mdl:rounded-l-xl ${path === '/profile' ? 'active' : ''}`}>
                 Profile
             </Link>
             {isAdmin && (
                 <>
                     <Link 
                     href={"/categories"}
-                    className={`border border-mainText ${path === '/profile' ? 'active' : ''}`}
+                    className={`border border-accentBg ${path === '/profile' ? 'active' : ''}`}
                     >
                         Blockchain
                     </Link>
                     <Link 
                     href={"/menu-items"}
-                    className={`border border-mainText ${path === '/profile' ? 'active' : ''}`}
+                    className={`border border-accentBg ${path === '/profile' ? 'active' : ''}`}
                     >
                         NFT
                     </Link>
                     <Link 
                     href={'/users'}
-                    className={`border border-mainText ${path === '/profile' ? 'active' : ''}`}
+                    className={`border border-accentBg ${path === '/profile' ? 'active' : ''}`}
                     >
                         Users
                     </Link>
@@ -40,7 +40,7 @@ const UserTabs = ({ isAdmin }) => {
             )}
              <Link 
                 href={'/orders'}
-                className={`border-2 border-mainText xs:rounded-b-xl mdl:rounded-l-none mdl:rounded-r-xl ${path === '/wallet' ? 'active' : ''}`}
+                className={`border-2 border-accentBg xs:rounded-b-xl mdl:rounded-l-none mdl:rounded-r-xl ${path === '/wallet' ? 'active' : ''}`}
                 >
                     Wallet
             </Link>
