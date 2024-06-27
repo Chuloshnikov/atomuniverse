af399611e4fd9103101c7bb9d03a9fd4cd504abe
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-06-27 10:28:49 +0300

    done

---
+++ src/app/marketplace/nft/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NftPage() {
+  return (
+    <div>nft page</div>
+  )
+}
---
+++ src/app/marketplace/tokens/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function TokensPage() {
+  return (
+    <div>
+      <Link href={"/marketplace/nft"}>nft</Link>
+      
+      tokens
+    </div>
+  )
+}
