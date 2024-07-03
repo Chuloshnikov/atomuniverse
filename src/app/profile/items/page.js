4501c838300a2cef369e3c89e870985423ac73f2
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-07-03 17:33:52 +0300

    done

---
+++ src/app/profile/items/page.js
@@ -0,0 +1,18 @@
+import UserTabs from '@/components/profile/UserTabs'
+import React from 'react'
+
+export default function CreatedItems() {
+  return (
+    <section
+    className="mt-12 p-4"
+    >
+        <UserTabs/>
+        <div
+        className=''
+        >
+        <h2>All items</h2>
+            
+        </div>
+    </section>
+  )
+}
---
+++ src/app/profile/nft/page.js
@@ -0,0 +1,18 @@
+import UserTabs from '@/components/profile/UserTabs'
+import React from 'react'
+
+export default function CreatedNfts() {
+  return (
+    <section
+    className="mt-12 p-4"
+    >
+        <UserTabs/>
+        <div
+        className=''
+        >
+        <h2>All nft's</h2>
+            
+        </div>
+    </section>
+  )
+}
