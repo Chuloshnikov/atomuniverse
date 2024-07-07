875e312267071feae899da5e4e7c7445296afe0b
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-07-07 18:12:24 +0300

    done

--- src/app/marketplace/tokens/page.js
+++ src/app/marketplace/tokens/page.js
@@ -1,12 +1,16 @@
-import React from 'react'
 import Link from 'next/link'
 
 export default function TokensPage() {
   return (
-    <div>
-      <Link href={"/marketplace/nft"}>nft</Link>
-      
-      tokens
+    <div className='max-w-md mx-auto mt-12 p-4'>
+      <div className='mx-auto flex items-center justify-center rounded-md'>
+        <div  className="bg-accentBg text-white py-2 px-4 text-center w-full border-2 border-accentBg rounded-l-md">
+          tokens
+        </div>
+        <Link href={"/marketplace/nft"} className="bg-black text-white py-2 px-4 text-center w-full border-2 border-accentBg rounded-r-md">
+            nft
+        </Link>
+      </div>
     </div>
   )
 }
