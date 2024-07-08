324706545e91c2fceacbc47c4d3cf8ca069b09bd
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-07-08 17:38:37 +0300

    done

--- src/app/marketplace/tokens/page.js
+++ src/app/marketplace/tokens/page.js
@@ -1,6 +1,22 @@
-import Link from 'next/link'
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function TokensPage() {
+
+  const [vouchers, setVouchers] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/items')
+      .then(response => response.json())
+      .then(data => {
+        const filteredVouchers = data.filter(item => item.category === 'voucher');
+        setVouchers(filteredVouchers);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className='max-w-2xl mx-auto mt-12 p-4 pb-12'>
       <div className='mx-auto flex items-center justify-center rounded-md'>
@@ -10,6 +26,16 @@ export default function TokensPage() {
         <Link href={"/marketplace/nft"} className="bg-black text-white py-2 px-4 text-center w-full border-2 border-accentBg rounded-r-md">
             nft
         </Link>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-8">
+          {vouchers && vouchers.map(voucher => (
+            <div className="bg-black rounded-md flex justify-center items-center p-4">
+              <div className="flex flex-col gap-2 text-center">
+                <Image src={voucher.image} width={200} height={100} className="rounded-md" alt={voucher.name}/>
+                <h3 className="font-semibold">{voucher.name}</h3>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   )
