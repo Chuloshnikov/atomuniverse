fa1e12749e9bedd18d942ccb65e4092dd0e82655
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-07-19 15:09:02 +0300

    done

--- src/components/marketplace/MarketplaceItemBox.jsx
+++ src/components/marketplace/MarketplaceItemBox.jsx
@@ -1,8 +1,11 @@
 "use client"
+import Image from 'next/image';
 import React from 'react'
 import { FaWindowClose } from 'react-icons/fa';
 
 const MarketplaceItemBox = ({itemInfo, toggle}) => {
+
+  console.log(itemInfo)
   return (
     <div className='z-10 fixed inset-0 bg-black/80 flex flex-col text-white'>
       <div className='flex justify-end items-end p-4'>
@@ -10,9 +13,11 @@ const MarketplaceItemBox = ({itemInfo, toggle}) => {
           <FaWindowClose className="h-7 w-7 text-accentBg hover:text-smouthText duration-200"/>
       </button>
       </div>
-      <div className=''>
-        <div>
-          {itemInfo.name}
+      <div className='flex items-center justify-center px-2'>
+        <div className='bg-black rounded-md overflow-hidden text-center'>
+          <Image src={itemInfo.image} width={500} height={250} alt={'itemimage'}/>
+          <h3 className='text-2xl text-smouthText font-semibold m-4'>{itemInfo.name}</h3>
+          <p className='text-lg text-smouthText font-semibold m-4'>price: <span>{itemInfo.price} tokens</span></p>
         </div>
       </div>
     </div>
