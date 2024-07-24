f0e515902709f6ebc5b0e71108ddad1fda374264
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-07-24 12:10:47 +0300

    done

--- src/components/footer/FooterInput.jsx
+++ src/components/footer/FooterInput.jsx
@@ -2,6 +2,22 @@
 import { useState } from 'react'
 
 const FooterInput = () => {
+
+  const [email, setEmail] = useState('');
+
+  async function getInTouch(e) {
+    e.preventDefault();
+    const response = await fetch('/api/get-in-touch' {
+      method: 'POST',
+      body: JSON.stringify(email),
+      headers: {'Content-Type': 'application/json'}
+    })
+    if (response.ok) {
+      setEmail('');
+  } else {
+      setEmail('Error');
+  }
+  }
   return (
     <div>
         <h5 className='text-4xl'>Let’s stay in touch.</h5>
@@ -9,10 +25,12 @@ const FooterInput = () => {
         className='border-2 mt-2 border-mainText border-2 rounded-3xl max-w-[377px] flex gap-2 items-center justify-between'
         >
             <input
-            className='text-sm lg:text-base text-black py-2 px-5 rounded-3xl bg-black w-full text-mainText'
+            onChange={e => setEmail(e.target.value)}
+            className='text-sm lg:text-base py-2 px-5 rounded-3xl bg-black w-full text-mainText'
             placeholder='email'
             />
             <button
+            type='submit'
             className='py-3 px-5 border-2 border-mainText rounded-3xl text-xs lg:text-sm duration-300 font-semibold'
             >
               Submit
